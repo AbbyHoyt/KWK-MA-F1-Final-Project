0e5b7a9f5c140479d474f49b1e2f55ba86490be3
Abby Hoyt <abby.hoyt@comcast.net>
2023-07-31 14:37:21 -0700

    Initial Commit

---
+++ KWK MA F1 Final Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KWK MA F1 Final Project
+//
+//  Created by Abby Hoyt on 7/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KWK MA F1 Final Project/KWK_MA_F1_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_MA_F1_Final_ProjectApp.swift
+//  KWK MA F1 Final Project
+//
+//  Created by Abby Hoyt on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_MA_F1_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
