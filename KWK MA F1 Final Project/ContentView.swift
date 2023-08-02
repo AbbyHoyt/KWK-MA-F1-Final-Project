e18b046a67512e36b42bde8df8e27b21ba215a96
Abby Hoyt <abby.hoyt@comcast.net>
2023-08-02 15:27:32 -0700

    Added NavigationLink and NavigationStack to one page, fixed ScrollView.

--- KWK MA F1 Final Project/ContentView.swift
+++ KWK MA F1 Final Project/ContentView.swift
@@ -18,73 +18,72 @@ struct ContentView: View {
     let brown = Color(red: 0.459, green: 0.247, blue: 0.196)
     
     var body: some View {
+        NavigationStack {
             ZStack {
                 // Background Color, Ignore Safe Area
                 coral
                     .ignoresSafeArea()
                 ScrollView {
-                
-                VStack {
-                    Image("logo")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(250)
-                        .padding(5)
-                        .frame(width: 200.0, height: 200.0)
-                    
-                    // Spacer()
-                    
                     VStack {
-                        HStack {
-                            Text("75%")
-                                .font(.custom("Codec Pro ExtraBold", size: 70))
-                                .foregroundColor(red)
+                        Image("logo")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(250)
+                            .padding(5)
+                            .frame(width: 200.0, height: 200.0)
+                        
+                        // Spacer()
+                        
+                        VStack {
+                            HStack {
+                                Text("75%")
+                                    .font(.custom("Codec Pro ExtraBold", size: 70))
+                                    .foregroundColor(red)
+                                
+                                Text("OF CORAL REEFS THREATENED BY 2050")
+                                    .font(.custom("Aileron Bold Italic", size: 25))
+                                    .foregroundColor(red)
+                            }
                             
-                            Text("OF CORAL REEFS THREATENED BY 2050")
-                                .font(.custom("Aileron Bold Italic", size: 25))
-                                .foregroundColor(red)
-                        }
-                        
-                        .padding()
-                        
-                        HStack {
-                            Text("25%")
-                                .font(.custom("Codec Pro ExtraBold", size: 70))
-                                .foregroundColor(red)
+                            .padding()
                             
-                            Text("OF REEFS DAMAGED BEYOND REPAIR")
-                                .font(.custom("Aileron Bold Italic", size: 25))
-                                .foregroundColor(red)
-                        }
-                        .padding()
-                        
-                        HStack {
-                            Text("90%")
-                                .font(.custom("Codec Pro ExtraBold", size: 70))
-                                .foregroundColor(red)
+                            HStack {
+                                Text("25%")
+                                    .font(.custom("Codec Pro ExtraBold", size: 70))
+                                    .foregroundColor(red)
+                                
+                                Text("OF REEFS DAMAGED BEYOND REPAIR")
+                                    .font(.custom("Aileron Bold Italic", size: 25))
+                                    .foregroundColor(red)
+                            }
+                            .padding()
                             
-                            Text("OF DAMAGE WAS DONE BY HUMANS")
-                                .font(.custom("Aileron Bold Italic", size: 25))
-                                .foregroundColor(red)
-                        }
-                        .padding()
-                        
-                        
-                      /*  Button("how can I help?") {
+                            HStack {
+                                Text("90%")
+                                    .font(.custom("Codec Pro ExtraBold", size: 70))
+                                    .foregroundColor(red)
+                                
+                                Text("OF DAMAGE WAS DONE BY HUMANS")
+                                    .font(.custom("Aileron Bold Italic", size: 25))
+                                    .foregroundColor(red)
+                            }
+                            .padding()
                             
+                            NavigationLink(destination: LearnView()){
+                                Text("how can I help?")
+                                    .font(.custom("Codec Pro ExtraBold", size: 20))
+                                    .foregroundColor(cream)
+                                    .padding(10)
+                                    .background(brown)
+                                    .cornerRadius(20)
+                                    .fontWeight(.semibold)
+                                    .navigationTitle("Home")
+                                    .navigationBarTitleDisplayMode(.inline)
+                                    .navigationBarHidden(true)
+                            }
                         }
-                        .font(.custom("Codec Pro ExtraBold", size: 20))
-                        .padding(10)
-                        .foregroundColor(cream)
-                        .background(brown)
-                        .cornerRadius(20)
-                        .padding(.bottom, 10.0) */
-                        
-                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                        
+                        Spacer()
                     }
-                    Spacer()
-                    
                 }
             }
         }
