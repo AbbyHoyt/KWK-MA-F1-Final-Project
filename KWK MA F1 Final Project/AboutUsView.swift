81944e944c90bb47a0e1b9acd0025938da528aec
Abby Hoyt <abby.hoyt@comcast.net>
2023-08-02 17:45:46 -0700

    Fixed About Us page bugs.

--- KWK MA F1 Final Project/AboutUsView.swift
+++ KWK MA F1 Final Project/AboutUsView.swift
@@ -16,7 +16,6 @@ struct AboutUsView: View {
     let cream = Color(red: 0.945, green: 0.875, blue: 0.808)
     let brown = Color(red: 0.459, green: 0.247, blue: 0.196)
     
-    
     var body: some View {
         ZStack {
             // Background Color, Ignore Safe Area
@@ -27,29 +26,31 @@ struct AboutUsView: View {
                 
                 VStack {
                     Text("about us")
-                        .font(.custom("Codec Pro ExtraBold", size: 35))
-                        .foregroundColor(red)
-                        .multilineTextAlignment(.center)
-                        .padding(10)
-                    
-                    Text("Learn about us!")
-                        .font(.custom("Aileron Bold", size: 20))
-                        .foregroundColor(red)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    
+                         .font(.custom("Codec Pro ExtraBold", size: 35))
+                         .foregroundColor(red)
+                         .multilineTextAlignment(.center)
+                         .padding(10)
+                     
+                     Text("Learn about us!")
+                         .font(.custom("Aileron Bold", size: 20))
+                         .foregroundColor(red)
+                         .multilineTextAlignment(.center)
+                         .padding(10)
+                }
+                
+                VStack {
                     Image("placeholder")
                         .resizable(resizingMode: .stretch)
                         .aspectRatio(contentMode: .fit)
                         .cornerRadius(250)
                         .padding(5)
                         .frame(width: 200.0, height: 200.0)
-                
+                    
                     Text("Hailey")
                         .font(.custom("Aileron Bold Italic", size: 25))
                         .foregroundColor(red)
                         .textCase(.uppercase)
-                    
+                                        
                     Text("About Hailey...")
                         .font(.custom("Aileron Bold", size: 20))
                         .foregroundColor(red)
@@ -60,34 +61,31 @@ struct AboutUsView: View {
                         .cornerRadius(250)
                         .padding(5)
                         .frame(width: 200.0, height: 200.0)
-                
+                    
                     Text("Abby")
                         .font(.custom("Aileron Bold Italic", size: 25))
                         .foregroundColor(red)
                         .textCase(.uppercase)
-                    
+                                        
                     Text("About Abby...")
                         .font(.custom("Aileron Bold", size: 20))
                         .foregroundColor(red)
                     
-                 /*   Image("placeholder")
+                    Image("placeholder")
                         .resizable(resizingMode: .stretch)
                         .aspectRatio(contentMode: .fit)
                         .cornerRadius(250)
                         .padding(5)
                         .frame(width: 200.0, height: 200.0)
-                
+                    
                     Text("Charlotte")
                         .font(.custom("Aileron Bold Italic", size: 25))
                         .foregroundColor(red)
                         .textCase(.uppercase)
-                    
+                                        
                     Text("About Charlotte...")
                         .font(.custom("Aileron Bold", size: 20))
-                        .foregroundColor(red) */
-                    
-                    
-                    Spacer()
+                        .foregroundColor(red)
                 }
             }
         }
